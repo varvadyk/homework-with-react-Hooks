9acaeff35daf6870647909e573c522a33cbdaa3a
Diana <diana_ivanivna@kr.net>
2020-04-14 15:55:03 +0300

    Updates

---
+++ precache-manifest.2476869fba2bfb168bbfe55c5d3b55ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8edab7566e0d03544718a99092e1b4",
+    "url": "/homework-with-react-Hooks/index.html"
+  },
+  {
+    "revision": "bbd0f9eb7819fe7622ff",
+    "url": "/homework-with-react-Hooks/static/css/main.3cae502f.chunk.css"
+  },
+  {
+    "revision": "424fb0c119fea66c11ba",
+    "url": "/homework-with-react-Hooks/static/js/2.09b8c727.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework-with-react-Hooks/static/js/2.09b8c727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd0f9eb7819fe7622ff",
+    "url": "/homework-with-react-Hooks/static/js/main.1642319e.chunk.js"
+  },
+  {
+    "revision": "5e3e52a1d49de273ba50",
+    "url": "/homework-with-react-Hooks/static/js/runtime-main.3915b8cb.js"
+  }
+]);
