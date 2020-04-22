e5e16429acc7f17a37a6824770b2871e696128c3
varvadyk <diana_ivanivna@ukr.net>
2020-04-22 21:53:28 +0300

    fix syntax errors

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,13 +1,12 @@
 import React, { useState,useEffect } from 'react';
 import './Timer.css';
-import {TiMediaPlay}from "react-icons/ti";
-import {TiMediaPause}from "react-icons/ti";
-const Timer=({time,autostart,step,onTick,onTimeStart,onTimePause,onTimeEnd})=>{
-   
+import {TiMediaPlay ,TiMediaPause}from "react-icons/ti";
+
+const Timer=({time,autostart,step,onTick,onTimeStart,onTimePause,onTimeEnd})=>{  
 const [ count,setCount]=useState(time);
 const [activeTimer,setActive]=useState(false);
 
-function ChangeActive(){
+function changeActive(){
     setActive(!activeTimer)
     activeTimer ? onTimePause():onTimeStart();
 
@@ -16,33 +15,31 @@ function ChangeActive(){
 useEffect(()=>{
     
     if(activeTimer ||autostart){
-       
-    const id=window.setInterval(()=>{
+    const id=setInterval(()=>{
         setCount( count=>count-1);
         onTick(count);
         },step);
         if(count===0){
             onTimeEnd();
-            ChangeActive()
+            changeActive()
            
         }
-    return ()=>window.clearInterval(id);
+    return ()=>clearInterval(id);
 }
-return undefined;
-},[activeTimer, autostart, step, onTick, count, onTimeStart, onTimeEnd, ChangeActive]);
+},[activeTimer, autostart, step, onTick, count, onTimeStart, onTimeEnd, changeActive]);
 return(
     <div className="buttons_content">
         <div className="timer">{count}</div>
         { activeTimer
         ? (
-        <button className="button_start" onClick={() => ChangeActive(true)}><span><TiMediaPause /></span></button>
+        <button className="button_start" onClick={() => changeActive(true)}><span><TiMediaPause /></span></button>
         )
         :(
-            
-            <button className="button_start" onClick={() => ChangeActive(false)}><span><TiMediaPlay /></span></button>
+         <button className="button_start" onClick={() => changeActive(false)}><span><TiMediaPlay /></span></button>
         )
         }  
     </div>
 );
 };
 export default Timer;
+
